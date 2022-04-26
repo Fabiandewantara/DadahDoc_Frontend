ae7f8a5eeee00ccd5d1b0136b160e6247ddff4c3
Fabian Dewantara Santonie <fabian.dewantara20@gmail.com>
2022-04-26 12:26:33 +0700

    Login Form

--- web-reactku/src/components/Menu.js
+++ web-reactku/src/components/Menu.js
@@ -7,6 +7,7 @@ const Menu = () => {
       <li><Link to="/">Home</Link></li>
       <li><Link to="/works">Works</Link></li>
       <li><Link to="/about">About</Link></li>
+      <li><Link to="/login">Login</Link></li>
     </ul>
   )
 }
