21096adc379021808d08bb7112691806319b7940
Fabian Dewantara Santonie <fabian.dewantara20@gmail.com>
2022-04-26 15:01:06 +0700

    logo navbar

--- web-reactku/src/components/Menu.js
+++ web-reactku/src/components/Menu.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from "../logonavbar.png"
+import "../styles/Menu.css"
 
 const Menu = () => {
   return (
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/works">Works</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/login">Login</Link></li>
-      <li><Link to="/AdminMain">Buat cek aja Admin main nya Sebelum ada fungsi login</Link></li>
-      <li><Link to="/DoctorMain">Buat cek aja Doctor main nya Sebelum ada fungsi login</Link></li>
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+      <div class="logo">
+        <img src={Logo} />
+      </div>
+    </nav>
+    // <ul>
+    //   <li><Link to="/">Home</Link></li>
+    //   <li><Link to="/works">Works</Link></li>
+    //   <li><Link to="/about">About</Link></li>
+    //   <li><Link to="/login">Login</Link></li>
+    //   <li><Link to="/AdminMain">Buat cek aja Admin main nya Sebelum ada fungsi login</Link></li>
+    //   <li><Link to="/DoctorMain">Buat cek aja Doctor main nya Sebelum ada fungsi login</Link></li>
+    // </ul>
   )
 }
 
---
+++ web-reactku/src/components/UserMain.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { TransitionGroup } from "react-transition-group";
+
+export default function UserMain(){
+    <TransitionGroup
+      transitionName="worksTransition"
+      transitionAppear={true}
+      transitionAppearTimeout={500}
+      transitionEnter={false}
+      transitionLeave={false}>
+          <nav class="">
+
+          </nav>
+      </TransitionGroup>
+}
