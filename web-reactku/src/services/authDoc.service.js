146737179809fe708117802118282fe8f217e843
Ananda Novi Prasetya <cut3.chorom@gmail.com>
2022-04-27 17:05:02 +0700

    Auth controller token

--- web-reactku/src/services/authDoc.service.js
+++ web-reactku/src/services/authDoc.service.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:8080/admin/";
+const user = JSON.parse(localStorage.getItem('user'));
+const token = user.accessToken
 
 class AuthServiceDoctor {
-
   register(username, email, password,fullName,birthDate,scheduleId) {
-    return axios.post(API_URL + "signup", {
+    return axios.post(API_URL + "signup", { headers: {'Authorization': `Bearer ${token}`} }, {
       username,
       email,
       password,
       fullName,
       birthDate,
       scheduleId
+
+      // return axios.get(API_URL + 'user', { headers: authHeader() });
     });
   }
   
